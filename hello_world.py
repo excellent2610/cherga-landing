677d8f3dfafd24769ec034f44f1beb34f61091e3
excellent2610 <shkvotersb@gmail.com>
2026-05-23 18:39:14 +0300

    Add bot source

---
+++ hello_world.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""
+Простий Hello World на Python
+"""
+
+def main():
+    print("Hello, World!")
+    print("Привіт, Світ!")
+
+if __name__ == "__main__":
+    main()
